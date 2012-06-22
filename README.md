dce2eab88a5abf8aab8a759544f326dea997f065
Laurence Cope <info@amitywebsolutions.co.uk>
2012-06-22 10:20:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Description
+
+An Expression Engine Fieldtype.
+
+It can be confusing to have a Pages URI and a URL Title for customers. They may ask why both, and you have to get all technical. Most situations will require the Pages URI to be the same as the URL Title. 
+
+Therefore the Auto Pages URI fieldtype automatically converts your page Title into the Pages URI based on the Title. To put it another way, it will copy the URL Title to the Pages URI automatically when you enter the entry Title. It is actually based on the process to create the URI Title.
+
+This saves you or the customer entering the Pages URI which can be confusing due to having a URL Title also. 
+
+## Installation
+
+* Unzip and upload the folder auto_pages_uri to your system/expressionengine/third_party/ folder.
+* Go to Add-Ons Fieldtypes and click Install next to the fieldtype Auto Pages URI
+* Add the fieldtype to your channel field group that you want the Pages URI automatically created
+
+The fieldtype will now copy the URL Title to the Pages URI automatically 
+
+## Optional Additional Configuration
+
+The fieldtype will display as a field in the channel entry, yet it requires no data to be entered. Therfore it is recommended to Hide the field in the channel entry by clicking Show Toolbar and clicking the eye icon for Auto Pages URI to hide it. It will still work. 
+
+## Future Improvements
+
+The Pages URI will only act once, on the initial page entry. Once you edit a page and change the Title, or URL Title, it will not update the Pages URL. You will need to manually click the Pages tab and edit it. So an idea for future developments is to provide an option in the fieldtype to keep the entry or update everytime. 
+
+## Bugs & Feature Requests
+
+[Issue tracker](http://github.com/amityweb/auto_pages_url/issues)
+
+Before reporting bugs or requesting any features, please check that it does not already exist.
+
+## Current version
+
+1.0
---
+++ ft.auto_pages_uri.php
@@ -0,0 +1,112 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Auto Pages URI Fieldtype
+*
+* @package		AutoPagesURI
+* @version		1.0
+* @author		Laurence Cope, Amity Web Solutions Ltd
+* @copyright 	Copyright (c) 2011 Amity Web Solutions Ltd <http://www.amitywebsolutions.co.uk>
+*/
+
+class Auto_pages_uri_ft extends EE_Fieldtype {
+
+	var $info = array(
+		'name'		=> 'Auto Pages URI',
+		'version'	=> '1.0'
+	);
+	
+	// --------------------------------------------------------------------
+
+	/**
+	* Display Field on Publish
+	*
+	* @access	public
+	* @return	field html
+	*
+	*/
+	function display_field()
+	{
+	
+		$this->EE->cp->add_to_head('
+			<script type="text/javascript">
+			
+			$(function() {
+				$("#title").keyup(function() { // Maybe use blur instead??
+					
+					var entry_id = "' . (isset($_GET['entry_id']) ? ''.$_GET['entry_id'].'' : '') . '";
+					if(!entry_id)
+					{
+						var page_url = ee_page_uri($("#title"));
+						$("#pages__pages_uri").val(page_url);
+					}
+				
+				});;
+				
+				$("#pages__pages_uri").keyup(function() { // Maybe use blur instead??
+					var url_title = ee_page_uri($("#pages__pages_uri"));
+					$("#url_title").val(url_title);
+				
+				});;
+				
+				$("#url_title").keyup(function() { // Maybe use blur instead??
+					var pages__pages_uri = ee_page_uri($("#url_title"));
+					$("#pages__pages_uri").val(pages__pages_uri);
+				
+				});;
+				
+			});
+
+		    function ee_page_uri(title)
+		    {
+
+		            var b = EE.publish.default_entry_title ? EE.publish.default_entry_title : "",
+		                c = EE.publish.word_separator ? EE.publish.word_separator : "_",
+		                h = EE.publish.foreignChars ? EE.publish.foreignChars : {},
+		                a = title.val() || "",
+		                i = RegExp(c + "{2,}", "g"),
+		                d = c !== "_" ? /\_/g : /\-/g,
+		                f = "",
+		                j = EE.publish.url_title_prefix ? EE.publish.url_title_prefix : "";
+		            b !== "" && title.attr("id") === "title" && a.substr(0, b.length) === b && (a = a.substr(b.length));
+		            a = (j + a).toLowerCase().replace(d, c);
+		            for (b = 0; b < a.length; b++) d = a.charCodeAt(b), d >= 32 && d < 128 ? f += a.charAt(b) : d in h && (f += h[d]);
+		            a = f.replace("/<(.*?)>/g", "");
+		            a = a.replace(/\s+/g, c);
+		            a = a.replace(/\//g, c);
+		            a = a.replace(/[^a-z0-9\-\._]/g, "");
+		            a = a.replace(/\+/g, c);
+		            a = a.replace(i, c);
+		            a = a.replace(/^[\-\_]|[\-\_]$/g, "");
+		            a = a.replace(/\.+$/g, "");
+	
+					return a;
+		    }
+								
+			</script>		
+			');
+
+
+		return false;
+	}
+	
+	// --------------------------------------------------------------------
+	
+	/**
+	* Install Fieldtype
+	*
+	* @access	public
+	* @return	default global settings
+	*
+	*/
+	function install()
+	{
+		return array(	
+	);
+}
+	
+	// --------------------------------------------------------------------
+	
+}
+
+/* End of file ft.auto_pages_uri.php */
+/* Location: ./system/expressionengine/third_party/pages_uri/ft.auto_pages_uri.php */
